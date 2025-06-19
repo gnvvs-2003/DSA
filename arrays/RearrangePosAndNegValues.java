7e8dbed991f5c93a1970bf219e8bd3a2662217bc
Zenitsu-2003 <161148525+gnvvs-2003@users.noreply.github.com>
2025-06-19 13:57:00 +0000

    reformatted function in Rearrange array elements pos+neg format

--- arrays/RearrangePosAndNegValues.java
+++ arrays/RearrangePosAndNegValues.java
@@ -1,4 +1,21 @@
 public class RearrangePosAndNegValues {
+    public static int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        int negative = 1;
+        int positive = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] > 0){
+                temp[positive] = nums[i];
+                positive = positive + 2;
+            }
+            else{
+                temp[negative] = nums[i];
+                negative = negative + 2;
+            }
+        }
+        return temp;
+    }
     public static int[] rearrange(int[] arr) {
         int n = arr.length;
         int[] pos = new int[n];
@@ -25,6 +42,10 @@ public class RearrangePosAndNegValues {
         int[] array = {3, 1, -2, -5, 2, -4};
         for (int i : rearrange(array)) {
             System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i : rearrangeArray(array)) {
+            System.out.print(i+" ");
         }
     }
 }
