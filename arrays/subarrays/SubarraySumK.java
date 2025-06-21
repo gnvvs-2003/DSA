48488a9008d59d0cf402d265e057c030f5509ccb
Zenitsu-2003 <161148525+gnvvs-2003@users.noreply.github.com>
2025-06-21 12:52:00 +0000

    count of sub arrays with sum = k - completed with optimal solution

--- arrays/subarrays/SubarraySumK.java
+++ arrays/subarrays/SubarraySumK.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumK {
     public static int countSubArrayWithSumK(int[]array,int target){
         int count = 0;
@@ -12,9 +15,32 @@ public class SubarraySumK {
         }
         return count;
     }
+    public static int countSubArrayWithSumKOptimal(int[] array, int target) {
+        int count = 0;
+        int prefixSum = 0;
+        int n = array.length;
+        Map<Integer, Integer> mapping = new HashMap<>();
+        mapping.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            prefixSum += array[i];
+            int remaining = prefixSum - target;
+            if (mapping.containsKey(remaining)) {
+                count += mapping.get(remaining);
+            } else {
+                count += 0;
+            }
+            if (mapping.containsKey(prefixSum)) {
+                mapping.put(prefixSum, mapping.get(prefixSum) + 1);
+            } else {
+                mapping.put(prefixSum, 1);
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         int[]array = {3, 1, 2, 4};
         int target = 6;
         System.out.println(countSubArrayWithSumK(array,target));
+        System.out.println(countSubArrayWithSumKOptimal(array,target));
     }
 }
